07172a8c8fd33e5f1ebeebd5a406998a9efef5d7
Ethan <embog669@learner.dvusd.org>
2022-08-16 08:21:07 -0700

    Added pokedex

---
+++ VB/Poke dex/Poke dex/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Name = New System.Windows.Forms.Label()
+        Me.Location = New System.Windows.Forms.Label()
+        Me.Gen = New System.Windows.Forms.Label()
+        Me.Type = New System.Windows.Forms.Label()
+        Me.namelabel = New System.Windows.Forms.Label()
+        Me.Locationlabel = New System.Windows.Forms.Label()
+        Me.Genlabel = New System.Windows.Forms.Label()
+        Me.Typelabel = New System.Windows.Forms.Label()
+        Me.TyranitarButton = New System.Windows.Forms.Button()
+        Me.PikachuButton = New System.Windows.Forms.Button()
+        Me.VenusaurButton = New System.Windows.Forms.Button()
+        Me.CharizardButton = New System.Windows.Forms.Button()
+        Me.BlastoiseButton = New System.Windows.Forms.Button()
+        Me.Pikachupicture = New System.Windows.Forms.PictureBox()
+        Me.blastoisepicture = New System.Windows.Forms.PictureBox()
+        Me.Tyranitarpicture = New System.Windows.Forms.PictureBox()
+        Me.venusaurpicture = New System.Windows.Forms.PictureBox()
+        Me.charizardpicture = New System.Windows.Forms.PictureBox()
+        Me.displaypicture = New System.Windows.Forms.PictureBox()
+        CType(Me.Pikachupicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.blastoisepicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Tyranitarpicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.venusaurpicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.charizardpicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.displaypicture, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Name
+        '
+        Me.Name.AutoSize = True
+        Me.Name.Location = New System.Drawing.Point(500, 95)
+        Me.Name.Name = "Name"
+        Me.Name.Size = New System.Drawing.Size(47, 16)
+        Me.Name.TabIndex = 6
+        Me.Name.Text = "Name:"
+        '
+        'Location
+        '
+        Me.Location.AutoSize = True
+        Me.Location.Location = New System.Drawing.Point(503, 184)
+        Me.Location.Name = "Location"
+        Me.Location.Size = New System.Drawing.Size(61, 16)
+        Me.Location.TabIndex = 7
+        Me.Location.Text = "Location:"
+        '
+        'Gen
+        '
+        Me.Gen.AutoSize = True
+        Me.Gen.Location = New System.Drawing.Point(505, 220)
+        Me.Gen.Name = "Gen"
+        Me.Gen.Size = New System.Drawing.Size(35, 16)
+        Me.Gen.TabIndex = 8
+        Me.Gen.Text = "Gen:"
+        '
+        'Type
+        '
+        Me.Type.AutoSize = True
+        Me.Type.Location = New System.Drawing.Point(505, 135)
+        Me.Type.Name = "Type"
+        Me.Type.Size = New System.Drawing.Size(42, 16)
+        Me.Type.TabIndex = 9
+        Me.Type.Text = "Type:"
+        '
+        'namelabel
+        '
+        Me.namelabel.AutoSize = True
+        Me.namelabel.Location = New System.Drawing.Point(554, 95)
+        Me.namelabel.Name = "namelabel"
+        Me.namelabel.Size = New System.Drawing.Size(10, 16)
+        Me.namelabel.TabIndex = 10
+        Me.namelabel.Text = " "
+        '
+        'Locationlabel
+        '
+        Me.Locationlabel.AutoSize = True
+        Me.Locationlabel.Location = New System.Drawing.Point(570, 184)
+        Me.Locationlabel.Name = "Locationlabel"
+        Me.Locationlabel.Size = New System.Drawing.Size(10, 16)
+        Me.Locationlabel.TabIndex = 11
+        Me.Locationlabel.Text = " "
+        '
+        'Genlabel
+        '
+        Me.Genlabel.AutoSize = True
+        Me.Genlabel.Location = New System.Drawing.Point(546, 220)
+        Me.Genlabel.Name = "Genlabel"
+        Me.Genlabel.Size = New System.Drawing.Size(10, 16)
+        Me.Genlabel.TabIndex = 12
+        Me.Genlabel.Text = " "
+        '
+        'Typelabel
+        '
+        Me.Typelabel.AutoSize = True
+        Me.Typelabel.Location = New System.Drawing.Point(553, 135)
+        Me.Typelabel.Name = "Typelabel"
+        Me.Typelabel.Size = New System.Drawing.Size(10, 16)
+        Me.Typelabel.TabIndex = 13
+        Me.Typelabel.Text = " "
+        '
+        'TyranitarButton
+        '
+        Me.TyranitarButton.Location = New System.Drawing.Point(948, 184)
+        Me.TyranitarButton.Name = "TyranitarButton"
+        Me.TyranitarButton.Size = New System.Drawing.Size(144, 23)
+        Me.TyranitarButton.TabIndex = 14
+        Me.TyranitarButton.Text = "Tyranitar"
+        Me.TyranitarButton.UseVisualStyleBackColor = True
+        '
+        'PikachuButton
+        '
+        Me.PikachuButton.Location = New System.Drawing.Point(901, 330)
+        Me.PikachuButton.Name = "PikachuButton"
+        Me.PikachuButton.Size = New System.Drawing.Size(144, 24)
+        Me.PikachuButton.TabIndex = 15
+        Me.PikachuButton.Text = "Pikachu"
+        Me.PikachuButton.UseVisualStyleBackColor = True
+        '
+        'VenusaurButton
+        '
+        Me.VenusaurButton.Location = New System.Drawing.Point(798, 184)
+        Me.VenusaurButton.Name = "VenusaurButton"
+        Me.VenusaurButton.Size = New System.Drawing.Size(144, 23)
+        Me.VenusaurButton.TabIndex = 16
+        Me.VenusaurButton.Text = "Venusaur"
+        Me.VenusaurButton.UseVisualStyleBackColor = True
+        '
+        'CharizardButton
+        '
+        Me.CharizardButton.Location = New System.Drawing.Point(789, 31)
+        Me.CharizardButton.Name = "CharizardButton"
+        Me.CharizardButton.Size = New System.Drawing.Size(144, 23)
+        Me.CharizardButton.TabIndex = 17
+        Me.CharizardButton.Text = "Charizard"
+        Me.CharizardButton.UseVisualStyleBackColor = True
+        '
+        'BlastoiseButton
+        '
+        Me.BlastoiseButton.Location = New System.Drawing.Point(939, 31)
+        Me.BlastoiseButton.Name = "BlastoiseButton"
+        Me.BlastoiseButton.Size = New System.Drawing.Size(144, 23)
+        Me.BlastoiseButton.TabIndex = 18
+        Me.BlastoiseButton.Text = "Blastoise"
+        Me.BlastoiseButton.UseVisualStyleBackColor = True
+        '
+        'Pikachupicture
+        '
+        Me.Pikachupicture.Image = Global.Poke_dex.My.Resources.Resources.pikachu
+        Me.Pikachupicture.Location = New System.Drawing.Point(901, 350)
+        Me.Pikachupicture.Name = "Pikachupicture"
+        Me.Pikachupicture.Size = New System.Drawing.Size(144, 100)
+        Me.Pikachupicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Pikachupicture.TabIndex = 5
+        Me.Pikachupicture.TabStop = False
+        '
+        'blastoisepicture
+        '
+        Me.blastoisepicture.Image = Global.Poke_dex.My.Resources.Resources.blastoise
+        Me.blastoisepicture.Location = New System.Drawing.Point(939, 51)
+        Me.blastoisepicture.Name = "blastoisepicture"
+        Me.blastoisepicture.Size = New System.Drawing.Size(144, 99)
+        Me.blastoisepicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.blastoisepicture.TabIndex = 4
+        Me.blastoisepicture.TabStop = False
+        '
+        'Tyranitarpicture
+        '
+        Me.Tyranitarpicture.Image = Global.Poke_dex.My.Resources.Resources.tyranitar
+        Me.Tyranitarpicture.Location = New System.Drawing.Point(948, 206)
+        Me.Tyranitarpicture.Name = "Tyranitarpicture"
+        Me.Tyranitarpicture.Size = New System.Drawing.Size(144, 99)
+        Me.Tyranitarpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Tyranitarpicture.TabIndex = 3
+        Me.Tyranitarpicture.TabStop = False
+        '
+        'venusaurpicture
+        '
+        Me.venusaurpicture.Image = Global.Poke_dex.My.Resources.Resources.Pokémon_Venusaur
+        Me.venusaurpicture.Location = New System.Drawing.Point(798, 206)
+        Me.venusaurpicture.Name = "venusaurpicture"
+        Me.venusaurpicture.Size = New System.Drawing.Size(144, 99)
+        Me.venusaurpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.venusaurpicture.TabIndex = 2
+        Me.venusaurpicture.TabStop = False
+        '
+        'charizardpicture
+        '
+        Me.charizardpicture.Image = Global.Poke_dex.My.Resources.Resources.Pokémon_Charzard
+        Me.charizardpicture.Location = New System.Drawing.Point(789, 51)
+        Me.charizardpicture.Name = "charizardpicture"
+        Me.charizardpicture.Size = New System.Drawing.Size(144, 99)
+        Me.charizardpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.charizardpicture.TabIndex = 1
+        Me.charizardpicture.TabStop = False
+        '
+        'displaypicture
+        '
+        Me.displaypicture.Location = New System.Drawing.Point(-7, 44)
+        Me.displaypicture.Name = "displaypicture"
+        Me.displaypicture.Size = New System.Drawing.Size(490, 395)
+        Me.displaypicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displaypicture.TabIndex = 0
+        Me.displaypicture.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1131, 604)
+        Me.Controls.Add(Me.BlastoiseButton)
+        Me.Controls.Add(Me.CharizardButton)
+        Me.Controls.Add(Me.VenusaurButton)
+        Me.Controls.Add(Me.PikachuButton)
+        Me.Controls.Add(Me.TyranitarButton)
+        Me.Controls.Add(Me.Typelabel)
+        Me.Controls.Add(Me.Genlabel)
+        Me.Controls.Add(Me.Locationlabel)
+        Me.Controls.Add(Me.namelabel)
+        Me.Controls.Add(Me.Type)
+        Me.Controls.Add(Me.Gen)
+        Me.Controls.Add(Me.Location)
+        Me.Controls.Add(Me.Name)
+        Me.Controls.Add(Me.Pikachupicture)
+        Me.Controls.Add(Me.blastoisepicture)
+        Me.Controls.Add(Me.Tyranitarpicture)
+        Me.Controls.Add(Me.venusaurpicture)
+        Me.Controls.Add(Me.charizardpicture)
+        Me.Controls.Add(Me.displaypicture)
+        Me.Name = "Form1"
+        Me.Text = "Pokedex"
+        CType(Me.Pikachupicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.blastoisepicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Tyranitarpicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.venusaurpicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.charizardpicture, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.displaypicture, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents displaypicture As PictureBox
+    Friend WithEvents charizardpicture As PictureBox
+    Friend WithEvents venusaurpicture As PictureBox
+    Friend WithEvents Tyranitarpicture As PictureBox
+    Friend WithEvents blastoisepicture As PictureBox
+    Friend WithEvents Pikachupicture As PictureBox
+    Friend WithEvents Name As Label
+    Friend WithEvents Location As Label
+    Friend WithEvents Gen As Label
+    Friend WithEvents Type As Label
+    Friend WithEvents namelabel As Label
+    Friend WithEvents Locationlabel As Label
+    Friend WithEvents Genlabel As Label
+    Friend WithEvents Typelabel As Label
+    Friend WithEvents TyranitarButton As Button
+    Friend WithEvents PikachuButton As Button
+    Friend WithEvents VenusaurButton As Button
+    Friend WithEvents CharizardButton As Button
+    Friend WithEvents BlastoiseButton As Button
+End Class
---
+++ VB/Poke dex/Poke dex/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub CharizardButton_Click(sender As Object, e As EventArgs) Handles CharizardButton.Click
+        displaypicture.Image = charizardpicture.Image
+        namelabel.Text = "charizard"
+        Typelabel.Text = "Fire and dragon"
+        Genlabel.Text = "1"
+        Locationlabel.Text = "Kanto"
+    End Sub
+
+    Private Sub BlastoiseButton_Click(sender As Object, e As EventArgs) Handles BlastoiseButton.Click
+        displaypicture.Image = blastoisepicture.Image
+        namelabel.Text = "Blastoise"
+        Typelabel.Text = "Water"
+        Genlabel.Text = "1"
+        Locationlabel.Text = "Kanto"
+    End Sub
+
+    Private Sub VenusaurButton_Click(sender As Object, e As EventArgs) Handles VenusaurButton.Click
+        displaypicture.Image = venusaurpicture.Image
+        namelabel.Text = "Venusaur"
+        Typelabel.Text = "Grass and poison"
+        Genlabel.Text = "1"
+        Locationlabel.Text = "Kanto"
+    End Sub
+
+    Private Sub TyranitarButton_Click(sender As Object, e As EventArgs) Handles TyranitarButton.Click
+        displaypicture.Image = Tyranitarpicture.Image
+        namelabel.Text = "Tyranitar"
+        Typelabel.Text = "Dark and rock"
+        Genlabel.Text = "2"
+        Locationlabel.Text = "Dust bowl"
+    End Sub
+
+    Private Sub PikachuButton_Click(sender As Object, e As EventArgs) Handles PikachuButton.Click
+        displaypicture.Image = Pikachupicture.Image
+        namelabel.Text = "Pikachu"
+        Typelabel.Text = "Electric"
+        Genlabel.Text = "1"
+        Locationlabel.Text = "Obsidian Fields"
+    End Sub
+End Class
