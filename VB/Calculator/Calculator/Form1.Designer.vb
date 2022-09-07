122204dbf15e0a217238b50946df8df6f9e3d23f
Ethan <embog669@learner.dvusd.org>
2022-09-07 07:45:55 -0700

    Added Calculator

---
+++ VB/Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,415 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.MultiplyButton = New System.Windows.Forms.Button()
+        Me.DividButton = New System.Windows.Forms.Button()
+        Me.PlusButton = New System.Windows.Forms.Button()
+        Me.MinusButton = New System.Windows.Forms.Button()
+        Me.EqualsButton = New System.Windows.Forms.Button()
+        Me.DisplayTextBox = New System.Windows.Forms.TextBox()
+        Me.ButtonDecimal = New System.Windows.Forms.Button()
+        Me.SquareRootButton = New System.Windows.Forms.Button()
+        Me.SinButton = New System.Windows.Forms.Button()
+        Me.CosButton = New System.Windows.Forms.Button()
+        Me.TanButton = New System.Windows.Forms.Button()
+        Me.PiButton = New System.Windows.Forms.Button()
+        Me.MemoryPlusButton = New System.Windows.Forms.Button()
+        Me.MemoryClearButton = New System.Windows.Forms.Button()
+        Me.MemoryButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button1.Font = New System.Drawing.Font("Lucida Console", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button1.Location = New System.Drawing.Point(27, 266)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(60, 43)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button3.Location = New System.Drawing.Point(183, 266)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(60, 43)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button4.Location = New System.Drawing.Point(27, 196)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(60, 43)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button2.Location = New System.Drawing.Point(108, 266)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(60, 43)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button5.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button5.Location = New System.Drawing.Point(108, 196)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(60, 43)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button6.Location = New System.Drawing.Point(183, 196)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(60, 43)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button7.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button7.Location = New System.Drawing.Point(27, 127)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(60, 43)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button8.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button8.Location = New System.Drawing.Point(108, 127)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(60, 43)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button9.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button9.Location = New System.Drawing.Point(183, 127)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(60, 43)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button0
+        '
+        Me.Button0.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button0.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Button0.Location = New System.Drawing.Point(27, 338)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(60, 43)
+        Me.Button0.TabIndex = 9
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = False
+        '
+        'ClearButton
+        '
+        Me.ClearButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.ClearButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClearButton.Location = New System.Drawing.Point(27, 398)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(60, 43)
+        Me.ClearButton.TabIndex = 10
+        Me.ClearButton.Text = "C"
+        Me.ClearButton.UseVisualStyleBackColor = False
+        '
+        'MultiplyButton
+        '
+        Me.MultiplyButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.MultiplyButton.Font = New System.Drawing.Font("Lucida Console", 15.0!)
+        Me.MultiplyButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MultiplyButton.Location = New System.Drawing.Point(119, 339)
+        Me.MultiplyButton.Name = "MultiplyButton"
+        Me.MultiplyButton.Size = New System.Drawing.Size(29, 43)
+        Me.MultiplyButton.TabIndex = 11
+        Me.MultiplyButton.Text = "*"
+        Me.MultiplyButton.UseVisualStyleBackColor = False
+        '
+        'DividButton
+        '
+        Me.DividButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.DividButton.Font = New System.Drawing.Font("Lucida Console", 10.0!)
+        Me.DividButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.DividButton.Location = New System.Drawing.Point(160, 338)
+        Me.DividButton.Name = "DividButton"
+        Me.DividButton.Size = New System.Drawing.Size(39, 43)
+        Me.DividButton.TabIndex = 12
+        Me.DividButton.Text = "/"
+        Me.DividButton.UseVisualStyleBackColor = False
+        '
+        'PlusButton
+        '
+        Me.PlusButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.PlusButton.Font = New System.Drawing.Font("Lucida Console", 10.0!)
+        Me.PlusButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PlusButton.Location = New System.Drawing.Point(106, 397)
+        Me.PlusButton.Name = "PlusButton"
+        Me.PlusButton.Size = New System.Drawing.Size(31, 43)
+        Me.PlusButton.TabIndex = 13
+        Me.PlusButton.Text = "+"
+        Me.PlusButton.UseVisualStyleBackColor = False
+        '
+        'MinusButton
+        '
+        Me.MinusButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.MinusButton.Font = New System.Drawing.Font("Lucida Console", 10.0!)
+        Me.MinusButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MinusButton.Location = New System.Drawing.Point(145, 338)
+        Me.MinusButton.Name = "MinusButton"
+        Me.MinusButton.Size = New System.Drawing.Size(29, 43)
+        Me.MinusButton.TabIndex = 14
+        Me.MinusButton.Text = "-"
+        Me.MinusButton.UseVisualStyleBackColor = False
+        '
+        'EqualsButton
+        '
+        Me.EqualsButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.EqualsButton.Font = New System.Drawing.Font("Lucida Console", 16.0!)
+        Me.EqualsButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.EqualsButton.Location = New System.Drawing.Point(216, 397)
+        Me.EqualsButton.Name = "EqualsButton"
+        Me.EqualsButton.Size = New System.Drawing.Size(33, 43)
+        Me.EqualsButton.TabIndex = 15
+        Me.EqualsButton.Text = "="
+        Me.EqualsButton.UseVisualStyleBackColor = False
+        '
+        'DisplayTextBox
+        '
+        Me.DisplayTextBox.Font = New System.Drawing.Font("Lucida Console", 16.0!)
+        Me.DisplayTextBox.Location = New System.Drawing.Point(27, 32)
+        Me.DisplayTextBox.Multiline = True
+        Me.DisplayTextBox.Name = "DisplayTextBox"
+        Me.DisplayTextBox.Size = New System.Drawing.Size(216, 60)
+        Me.DisplayTextBox.TabIndex = 16
+        '
+        'ButtonDecimal
+        '
+        Me.ButtonDecimal.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.ButtonDecimal.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ButtonDecimal.Location = New System.Drawing.Point(100, 337)
+        Me.ButtonDecimal.Name = "ButtonDecimal"
+        Me.ButtonDecimal.Size = New System.Drawing.Size(25, 43)
+        Me.ButtonDecimal.TabIndex = 17
+        Me.ButtonDecimal.Text = "."
+        Me.ButtonDecimal.UseVisualStyleBackColor = False
+        '
+        'SquareRootButton
+        '
+        Me.SquareRootButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.SquareRootButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SquareRootButton.Location = New System.Drawing.Point(187, 338)
+        Me.SquareRootButton.Name = "SquareRootButton"
+        Me.SquareRootButton.Size = New System.Drawing.Size(37, 43)
+        Me.SquareRootButton.TabIndex = 18
+        Me.SquareRootButton.Text = "√"
+        Me.SquareRootButton.UseVisualStyleBackColor = False
+        '
+        'SinButton
+        '
+        Me.SinButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.SinButton.Font = New System.Drawing.Font("Lucida Console", 6.0!)
+        Me.SinButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.SinButton.Location = New System.Drawing.Point(129, 397)
+        Me.SinButton.Name = "SinButton"
+        Me.SinButton.Size = New System.Drawing.Size(34, 43)
+        Me.SinButton.TabIndex = 21
+        Me.SinButton.Text = "Sin"
+        Me.SinButton.UseVisualStyleBackColor = False
+        '
+        'CosButton
+        '
+        Me.CosButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.CosButton.Font = New System.Drawing.Font("Lucida Console", 6.0!)
+        Me.CosButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.CosButton.Location = New System.Drawing.Point(160, 397)
+        Me.CosButton.Name = "CosButton"
+        Me.CosButton.Size = New System.Drawing.Size(34, 43)
+        Me.CosButton.TabIndex = 22
+        Me.CosButton.Text = "Cos"
+        Me.CosButton.UseVisualStyleBackColor = False
+        '
+        'TanButton
+        '
+        Me.TanButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.TanButton.Font = New System.Drawing.Font("Lucida Console", 6.0!)
+        Me.TanButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.TanButton.Location = New System.Drawing.Point(187, 397)
+        Me.TanButton.Name = "TanButton"
+        Me.TanButton.Size = New System.Drawing.Size(34, 43)
+        Me.TanButton.TabIndex = 23
+        Me.TanButton.Text = "Tan"
+        Me.TanButton.UseVisualStyleBackColor = False
+        '
+        'PiButton
+        '
+        Me.PiButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.PiButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.PiButton.Location = New System.Drawing.Point(216, 338)
+        Me.PiButton.Name = "PiButton"
+        Me.PiButton.Size = New System.Drawing.Size(33, 43)
+        Me.PiButton.TabIndex = 24
+        Me.PiButton.Text = "π"
+        Me.PiButton.UseVisualStyleBackColor = False
+        '
+        'MemoryPlusButton
+        '
+        Me.MemoryPlusButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.MemoryPlusButton.Font = New System.Drawing.Font("Lucida Console", 6.0!)
+        Me.MemoryPlusButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MemoryPlusButton.Location = New System.Drawing.Point(216, 89)
+        Me.MemoryPlusButton.Name = "MemoryPlusButton"
+        Me.MemoryPlusButton.Size = New System.Drawing.Size(34, 43)
+        Me.MemoryPlusButton.TabIndex = 25
+        Me.MemoryPlusButton.Text = "M+"
+        Me.MemoryPlusButton.UseVisualStyleBackColor = False
+        '
+        'MemoryClearButton
+        '
+        Me.MemoryClearButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.MemoryClearButton.Font = New System.Drawing.Font("Lucida Console", 6.0!)
+        Me.MemoryClearButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MemoryClearButton.Location = New System.Drawing.Point(43, 89)
+        Me.MemoryClearButton.Name = "MemoryClearButton"
+        Me.MemoryClearButton.Size = New System.Drawing.Size(34, 43)
+        Me.MemoryClearButton.TabIndex = 26
+        Me.MemoryClearButton.Text = "Mc"
+        Me.MemoryClearButton.UseVisualStyleBackColor = False
+        '
+        'MemoryButton
+        '
+        Me.MemoryButton.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.MemoryButton.Font = New System.Drawing.Font("Lucida Console", 6.0!)
+        Me.MemoryButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MemoryButton.Location = New System.Drawing.Point(119, 89)
+        Me.MemoryButton.Name = "MemoryButton"
+        Me.MemoryButton.Size = New System.Drawing.Size(34, 43)
+        Me.MemoryButton.TabIndex = 27
+        Me.MemoryButton.Text = "Mr"
+        Me.MemoryButton.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Calculator.My.Resources.Resources.claculator
+        Me.ClientSize = New System.Drawing.Size(284, 471)
+        Me.Controls.Add(Me.MemoryButton)
+        Me.Controls.Add(Me.MemoryClearButton)
+        Me.Controls.Add(Me.MemoryPlusButton)
+        Me.Controls.Add(Me.PiButton)
+        Me.Controls.Add(Me.TanButton)
+        Me.Controls.Add(Me.CosButton)
+        Me.Controls.Add(Me.SinButton)
+        Me.Controls.Add(Me.SquareRootButton)
+        Me.Controls.Add(Me.ButtonDecimal)
+        Me.Controls.Add(Me.DisplayTextBox)
+        Me.Controls.Add(Me.EqualsButton)
+        Me.Controls.Add(Me.MinusButton)
+        Me.Controls.Add(Me.PlusButton)
+        Me.Controls.Add(Me.DividButton)
+        Me.Controls.Add(Me.MultiplyButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button1)
+        Me.Font = New System.Drawing.Font("Lucida Console", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents MultiplyButton As Button
+    Friend WithEvents DividButton As Button
+    Friend WithEvents PlusButton As Button
+    Friend WithEvents MinusButton As Button
+    Friend WithEvents EqualsButton As Button
+    Friend WithEvents DisplayTextBox As TextBox
+    Friend WithEvents ButtonDecimal As Button
+    Friend WithEvents SquareRootButton As Button
+    Friend WithEvents SinButton As Button
+    Friend WithEvents CosButton As Button
+    Friend WithEvents TanButton As Button
+    Friend WithEvents PiButton As Button
+    Friend WithEvents MemoryPlusButton As Button
+    Friend WithEvents MemoryClearButton As Button
+    Friend WithEvents MemoryButton As Button
+End Class
---
+++ VB/Calculator/Calculator/Form1.vb
@@ -0,0 +1,90 @@
+﻿Public Class Form1
+    Dim Num1 As Decimal
+    Dim Num2 As Decimal
+    Dim Mem As Decimal
+    Dim Oper As String
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button9.Click, ButtonDecimal.Click, Button8.Click, Button7.Click, Button6.Click, Button5.Click, Button4.Click, Button3.Click, Button2.Click, Button1.Click, Button0.Click
+        DisplayTextBox.Text = DisplayTextBox.Text + sender.text
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DisplayTextBox.Text = Nothing
+    End Sub
+
+    Private Sub PlusButton_Click(sender As Object, e As EventArgs) Handles PlusButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        Oper = "+"
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub EqualsButton_Click(sender As Object, e As EventArgs) Handles EqualsButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num2)
+        If Oper = "-" Then
+            DisplayTextBox.Text = Num1 - Num2
+        End If
+        If Oper = "+" Then
+            DisplayTextBox.Text = Num1 + Num2
+        End If
+        If Oper = "/" Then
+            DisplayTextBox.Text = Num1 / Num2
+        End If
+        If Oper = "*" Then
+            DisplayTextBox.Text = Num1 * Num2
+        End If
+    End Sub
+
+    Private Sub MinusButton_Click(sender As Object, e As EventArgs) Handles MinusButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        Oper = "-"
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub DividButton_Click(sender As Object, e As EventArgs) Handles DividButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        Oper = "/"
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub MultiplyButton_Click(sender As Object, e As EventArgs) Handles MultiplyButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        Oper = "*"
+        DisplayTextBox.Clear()
+    End Sub
+
+    Private Sub SquareRootButton_Click(sender As Object, e As EventArgs) Handles SquareRootButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        DisplayTextBox.Text = Math.Sqrt(Num1)
+    End Sub
+
+    Private Sub SinButton_Click(sender As Object, e As EventArgs) Handles SinButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        DisplayTextBox.Text = Math.Sin(Num1)
+    End Sub
+
+    Private Sub CosButton_Click(sender As Object, e As EventArgs) Handles CosButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        DisplayTextBox.Text = Math.Cos(Num1)
+    End Sub
+
+    Private Sub TanButton_Click(sender As Object, e As EventArgs) Handles TanButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Num1)
+        DisplayTextBox.Text = Math.Tan(Num1)
+    End Sub
+
+    Private Sub PiButton_Click(sender As Object, e As EventArgs) Handles PiButton.Click
+        DisplayTextBox.Text = Math.PI
+    End Sub
+
+    Private Sub MemoryPlusButton_Click(sender As Object, e As EventArgs) Handles MemoryPlusButton.Click
+        Decimal.TryParse(DisplayTextBox.Text, Mem)
+    End Sub
+
+    Private Sub MemoryButton_Click(sender As Object, e As EventArgs) Handles MemoryButton.Click
+        DisplayTextBox.Text = Mem
+    End Sub
+
+    Private Sub MemoryClearButton_Click(sender As Object, e As EventArgs) Handles MemoryClearButton.Click
+        Mem = 0
+    End Sub
+End Class
