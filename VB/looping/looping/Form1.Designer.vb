99607e8ccd85c87e3e1a6041e434daa1e5c7c91a
Ethan <embog669@learner.dvusd.org>
2022-10-13 07:50:41 -0700

    Auto stash before merge of "main" and "origin/main"

--- VB/looping/looping/Form1.Designer.vb
+++ VB/looping/looping/Form1.Designer.vb
@@ -22,10 +22,39 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+<<<<<<< Updated upstream
         components = New System.ComponentModel.Container()
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Text = "Form1"
     End Sub
 
+=======
+        Me.Player = New System.Windows.Forms.PictureBox()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Player
+        '
+        Me.Player.Location = New System.Drawing.Point(311, 218)
+        Me.Player.Name = "Player"
+        Me.Player.Size = New System.Drawing.Size(100, 50)
+        Me.Player.TabIndex = 0
+        Me.Player.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Player)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Player As PictureBox
+>>>>>>> Stashed changes
 End Class
